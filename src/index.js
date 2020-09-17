d67d8ddd5a3957b0c841e73e52f7e3452931651c
Tcaprioli <Capriolityler@gmail.com>
2020-09-16 23:57:22 -0400

    imported App into index

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Counter from './counter';
+import App from './App'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counter />
+    {/* <Counter /> */}
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
