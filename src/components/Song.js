7edc5b6f9d19b851982e706f101be881d6c31ae3
lukelin1991 <lukelin1991@yahoo.com>
2020-10-03 12:05:34 -0400

    changed songjs to functional

--- src/components/Song.js
+++ src/components/Song.js
@@ -2,12 +2,11 @@ import React, {Component} from 'react'
 import record from './record.png'
 import "./song.css"
 
-export class Song extends Component{
-    render(){
-        return(
-            <div className="hvr-bob song">
-                <img className=" song-img" src={record}/>
-            </div>
-        )
-    }
+export const Song = ({ song }) => {
+    const { name, artist } = song
+    return(
+        <div className="hvr-bob song">
+            <img className=" song-img" src={record}/>
+        </div>
+    )
 }
